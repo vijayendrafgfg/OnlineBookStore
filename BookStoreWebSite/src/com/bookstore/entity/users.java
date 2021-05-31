6e86a260c28292f4d59a54f9d2885d37ce88fd9a
vijayendrafgfg <59336749+vijayendrafgfg@users.noreply.github.com>
2021-05-31 13:09:07 +0530

    first commit

---
+++ BookStoreWebSite/src/com/bookstore/entity/users.java
@@ -0,0 +1,55 @@
+package com.bookstore.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+
+public class users {
+
+	private Integer user_id;
+	private String email;
+	private String password;
+	private String full_name;
+
+	@Column(name = "user_id")
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	
+	public Integer getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Integer user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFull_name() {
+		return full_name;
+	}
+
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+
+}
